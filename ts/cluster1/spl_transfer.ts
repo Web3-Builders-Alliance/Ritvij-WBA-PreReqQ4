f26fd9d8f84659dd315529bd8234dd348b1c2d0b
Ritvij Kumar Sharma <ritvij2001@gmail.com>
2023-12-04 13:06:48 +0530

    :sparkles: Completed cluster 1 code

--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -1,28 +1,59 @@
-import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
+import {
+  Commitment,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+} from "@solana/web3.js";
+import wallet from "../wba-wallet.json";
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
+import base58 from "bs58";
 
 // We're going to import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+const bs = base58.decode(wallet);
+const keypair = Keypair.fromSecretKey(new Uint8Array(bs));
 
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("2rQKsxqdtvpxieGeqo3rs9yCyaf94yhoLA7nXW36KxA5");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("FKzt4ErLRAFUbVD5HRsFdR1vBYH6UHahxQ1fFocHVrGP");
 
 (async () => {
-    try {
-        // Get the token account of the fromWallet address, and if it does not exist, create it
+  try {
+    // Get the token account of the fromWallet address, and if it does not exist, create it
+    const fromAta = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey
+    );
 
-        // Get the token account of the toWallet address, and if it does not exist, create it
+    // Get the token account of the toWallet address, and if it does not exist, create it
+    const toAta = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      to
+    );
 
-        // Transfer the new token to the "toTokenAccount" we just created
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+    // Transfer the new token to the "toTokenAccount" we just created
+    const transferTx = await transfer(
+      connection,
+      keypair,
+      fromAta.address,
+      toAta.address,
+      keypair,
+      1_000_000
+    );
+    console.log(
+      `Check out your TX here:\n\nhttps://explorer.solana.com/tx/${transferTx}?cluster=devnet`
+    );
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
--- ts/cluster1/vault_close.ts
+++ ts/cluster1/vault_close.ts
@@ -12,11 +12,13 @@ import {
   Address,
   BN,
 } from "@coral-xyz/anchor";
-import { WbaVault, IDL } from "../programs/wba_vault";
+import { WbaVault, IDL } from "./programs/wba_vault";
 import wallet from "../wba-wallet.json";
+import base58 from "bs58";
 
 // Import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+const bs = base58.decode(wallet);
+const keypair = Keypair.fromSecretKey(new Uint8Array(bs));
 
 // Commitment
 const commitment: Commitment = "confirmed";
@@ -30,25 +32,33 @@ const provider = new AnchorProvider(connection, new Wallet(keypair), {
 });
 
 // Create our program
-const program = new Program<WbaVault>(IDL, "<address>" as Address, provider);
+const program = new Program<WbaVault>(
+  IDL,
+  "D51uEDHLbWAxNfodfQDv7qkp8WZtxrhi3uganGbNos7o" as Address,
+  provider
+);
 
 // Create a random keypair
-const vaultState = new PublicKey("<address>");
+const vaultState = new PublicKey("fwwwZggpfMqjPLCwPnvPtKMup3q16bs42nLw2RdZZVJ");
 
 // Create a random keypair
-// const closeVaultState = ???
+const closeVaultState = Keypair.generate();
 
 (async () => {
   try {
-    // const signature = await program.methods
-    // .closeAccount()
-    // .accounts({
-    //     ???
-    // })
-    // .signers([
-    //     keypair
-    // ]).rpc();
-    // console.log(`Close success! Check out your TX here:\n\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`);
+    const signature = await program.methods
+      .closeAccount()
+      .accounts({
+        owner: keypair.publicKey,
+        systemProgram: SystemProgram.programId,
+        vaultState: vaultState,
+        closeVaultState: closeVaultState.publicKey,
+      })
+      .signers([keypair])
+      .rpc();
+    console.log(
+      `Close success! Check out your TX here:\n\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`
+    );
   } catch (e) {
     console.error(`Oops, something went wrong: ${e}`);
   }
